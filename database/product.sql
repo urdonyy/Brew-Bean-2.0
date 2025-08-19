90daa2cb2c50974fa1dffd3ac0bffa6bc343e116
urdoninit <donaldoclarit0704@gmail.com>
2025-08-19 16:18:15 +0800

    :seedling: add image files and fix sql tables

--- database/product.sql
+++ database/product.sql
@@ -10,19 +10,19 @@ CREATE TABLE products (
     price DECIMAL(10,2)
 );
 
-INSERT INTO products (product_id, product_name, price) VALUES
-('Hot Coffee', 'Caramel Macchiato', 150),
-('Hot Coffee', 'Espresso', 120),
-('Hot Coffee', 'Cappucino', 180),
-('Hot Coffee', 'Caffe Misto', 120),
+INSERT INTO products (product_id, product_name, price, image_filename) VALUES
+('Hot Coffee', 'Caramel Macchiato', 150, 'caramel_macchiato.jpg'),
+('Hot Coffee', 'Espresso', 120, 'espresso.jpg'),
+('Hot Coffee', 'Cappucino', 180, 'cappucino.jpg'),
+('Hot Coffee', 'Caffe Misto', 120, 'caffe_misto.jpg'),
 
-('Cold Coffee', 'Cold Brew', 120),
-('Cold Coffee', 'Chocolate Cream', 170),
-('Cold Coffee', 'Raspberry Cream ', 200),
-('Cold Coffee', 'Salted Caramel Macchiato', 150),
+('Cold Coffee', 'Cold Brew', 120, 'cold_brew.jpg'),
+('Cold Coffee', 'Chocolate Cream', 170, 'chocolate_cream.jpg'),
+('Cold Coffee', 'Raspberry Cream ', 200, 'raspberry_cream.jpg'),
+('Cold Coffee', 'Salted Caramel Macchiato', 150, 'salted_caramel_macchiato.jpg'),
 
-('Non-Coffee', 'Lemon Tea', 150),
-('Non-Coffee', 'Ice Tea', 150),
-('Non-Coffee', 'Chai Latte', 180),
-('Non-Coffee', 'Matcha Latte', 200);
+('Non-Coffee', 'Lemon Tea', 150, 'lemon_tea.jpg'),
+('Non-Coffee', 'Ice Tea', 150, 'ice_tea.jpg'),
+('Non-Coffee', 'Chai Latte', 180, 'chai_latte.jpg'),
+('Non-Coffee', 'Matcha Latte', 200, 'matcha_latte.jpg');
 
