654fce13cd4a65c1b0c737e05d3740d498cd6e01
unknown <Lea Mae>
2025-09-05 14:01:25 +0800

    added home view

---
+++ public/homeView.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../src/styles/homeView.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="mainContent">
+        <!-- <div class="headerImg">
+            <img src="./assets/images/coffee_beans.png" alt="coffee beans">
+        </div> -->
+
+            <h1 class="logoName">Brew & Bean</h1>
+            <h2 class="fresh">Fresh Beans. Warm Vibes. Welcome Home.</h2>
+            <p class="begin">Begin your day with something meaningful—a cup worth savoring.</p>
+
+            <div class="buttons">
+                <button class="viewDemos" onclick="window.location.href='#'">View Demos</button>
+                <button class="started" onclick="window.location.href='../public/dashboard.php'">Let's Get Started</button>
+            </div>
+
+            <!-- <div class="cupOfCoffee">
+            <img src="../assets/images/cup_of_coffee.png" alt="cup of coffee">
+            </div> -->
+            
+            <!-- <div class="footerImg">
+                <img src="./assets/images/coffee_beans2.png" alt="coffee beans">
+            </div> -->
+        
+        </div>
+        
+</body>
+</html>
